4b328269f10664ff06655f5f24202e1a6b8a61de
LeviAckerman <2537791055@qq.com>
2021-04-09 11:52:32 +0800

    3
    3

---
+++ b/precache-manifest.c6c76b9a32fdc620347bfcc5e5906239.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a9fcf97d9be758674df7993ecb639b1",
+    "url": "img/home_bg.png"
+  },
+  {
+    "revision": "bafcfacc9092872e05a0c588a715c5d2",
+    "url": "img/login_bg.png"
+  },
+  {
+    "revision": "4b96417b48773aa372aa1151f3120748",
+    "url": "img/login_top.png"
+  },
+  {
+    "revision": "e29cd2eca1e119ae6662268be660c021",
+    "url": "index.html"
+  },
+  {
+    "revision": "e8d7883dde92f3e8ae103c5ae5d6d587",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "a544f2d96970a95ac0d9",
+    "url": "static/css/app.d192268f.css"
+  },
+  {
+    "revision": "32c33920c07279e7007c",
+    "url": "static/css/chunk-2263ae9c.25ede1d1.css"
+  },
+  {
+    "revision": "622ae1ada4894dc4b3a2",
+    "url": "static/css/chunk-2f3194dd.ef7fd4de.css"
+  },
+  {
+    "revision": "75901dd0752052d3da7b",
+    "url": "static/css/chunk-3f17d4f9.7546292b.css"
+  },
+  {
+    "revision": "42172e9c2edd13481191",
+    "url": "static/css/chunk-5e00efde.c8494ae9.css"
+  },
+  {
+    "revision": "7139ed3b4e72785a4363",
+    "url": "static/css/chunk-731b592a.0d37b0f7.css"
+  },
+  {
+    "revision": "c08bc349593c15ee5040",
+    "url": "static/css/chunk-87dc6eb2.e3391729.css"
+  },
+  {
+    "revision": "8c1121756ee6e39cfc5c",
+    "url": "static/css/chunk-ce5074d6.1e09773e.css"
+  },
+  {
+    "revision": "6fa1036cd58db1721b3e",
+    "url": "static/css/vendors~app.f97ba22d.css"
+  },
+  {
+    "revision": "1bd56f8ecaa2a69b64f8deb424968357",
+    "url": "static/fonts/iconfont.1bd56f8e.ttf"
+  },
+  {
+    "revision": "b0043d736097e4de3d841207042d08ef",
+    "url": "static/fonts/iconfont.b0043d73.woff"
+  },
+  {
+    "revision": "ea975d08629905fe621daf3b15a9649c",
+    "url": "static/fonts/iconfont.ea975d08.eot"
+  },
+  {
+    "revision": "0026ea27d09e9946a43a50a506cd8288",
+    "url": "static/img/401.0026ea27.gif"
+  },
+  {
+    "revision": "45e21c93b8f1cc0d3466ea59170f18e7",
+    "url": "static/img/404.45e21c93.png"
+  },
+  {
+    "revision": "050013b73243763cb1c31bd58881b15a",
+    "url": "static/img/iconfont.050013b7.svg"
+  },
+  {
+    "revision": "246f42477f0e93740d226e78b110597a",
+    "url": "static/img/login_top.246f4247.png"
+  },
+  {
+    "revision": "ba7ee6b7a3d25ebffdebce2eb704dc7c",
+    "url": "static/img/logo.ba7ee6b7.png"
+  },
+  {
+    "revision": "98bbc8a214b6b88b97435b001820d87c",
+    "url": "static/img/punch_clock.98bbc8a2.png"
+  },
+  {
+    "revision": "237d92679acc7fde8ce0f231eb110ede",
+    "url": "static/ip.config.js"
+  },
+  {
+    "revision": "a544f2d96970a95ac0d9",
+    "url": "static/js/app.fcd9d803.js"
+  },
+  {
+    "revision": "32c33920c07279e7007c",
+    "url": "static/js/chunk-2263ae9c.3ed55cc0.js"
+  },
+  {
+    "revision": "622ae1ada4894dc4b3a2",
+    "url": "static/js/chunk-2f3194dd.5fb41d36.js"
+  },
+  {
+    "revision": "75901dd0752052d3da7b",
+    "url": "static/js/chunk-3f17d4f9.97430c5e.js"
+  },
+  {
+    "revision": "42172e9c2edd13481191",
+    "url": "static/js/chunk-5e00efde.4dd657ee.js"
+  },
+  {
+    "revision": "7139ed3b4e72785a4363",
+    "url": "static/js/chunk-731b592a.76681da3.js"
+  },
+  {
+    "revision": "c08bc349593c15ee5040",
+    "url": "static/js/chunk-87dc6eb2.d5b11991.js"
+  },
+  {
+    "revision": "8c1121756ee6e39cfc5c",
+    "url": "static/js/chunk-ce5074d6.f554ae0a.js"
+  },
+  {
+    "revision": "6fa1036cd58db1721b3e",
+    "url": "static/js/vendors~app.5c2ef413.js"
+  }
+]);
---
+++ b/static/ip.config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  requestTimeOut: 2 * 60, // 请求超时时间  秒
+  projectPrefix: "HSH5", // 项目前缀- 在存储token的时候加上项目前缀，防止对个项目token被覆盖
+  // devUrl: "http://192.168.100.60:15558/",
+  devUrl: "http://120.78.201.189:15558/",
+  prodUrl: "http://120.78.201.189:15558/",
+  baseURL: "",
+}
